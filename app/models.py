f21ad3a08198e77572776fa3ebb2279b7c423dc4
Daniel Bailo <danbailoufms@gmail.com>
2020-11-13 16:48:44 -0300

    implemented hashed password for user

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from datetime import datetime
 import uuid
@@ -8,6 +9,12 @@ class User(db.Document):
     username = db.StringField(max_length=64, min_length=4, unique=True, required=True)
     password_hash = db.StringField(max_length=64, min_length=4, required=True)
     email = db.EmailField(max_length=128, min_length=4, unique=True, required=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"<User {self.username}>"
