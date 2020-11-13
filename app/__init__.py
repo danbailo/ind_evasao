d9eac48aa792d71693784fe4acb3b9eaa8f949e5
Daniel Bailo <danbailoufms@gmail.com>
2020-11-13 15:42:34 -0300

    flask shell, added variables in shell context to test the app

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,7 @@ app.config.from_object(Config())
 db = MongoEngine(app)
 
 from app import routes, models
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': models.User, 'Form': models.Form}
