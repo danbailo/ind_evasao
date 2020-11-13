c976192de1824d04df26451e18f3a2bf548676ae
Daniel Bailo <danbailoufms@gmail.com>
2020-11-12 22:37:59 -0300

    added mongodb config

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    MONGODB_SETTINGS = os.environ.get("DATABASE_URL") or\
+    {
+        'db': 'ind_evasao',
+    }
