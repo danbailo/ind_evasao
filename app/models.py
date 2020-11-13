5e32144c3f1e157d8145875524646f00a352c4b7
Daniel Bailo <danbailoufms@gmail.com>
2020-11-12 23:10:04 -0300

    added form model

--- app/models.py
+++ app/models.py
@@ -1,14 +1,32 @@
 from app import db
+from datetime import datetime
 import uuid
+
 
 class User(db.Document):
     _id = db.StringField(primary_key=True, required=True) #mongodb autoimplemnt this field
-    username = db.StringField(max_length=64, min_length=4, required=True, unique=True)
+    username = db.StringField(max_length=64, min_length=4, unique=True, required=True)
     password_hash = db.StringField(max_length=64, min_length=4, required=True)
-    email = db.EmailField(max_length=128, min_length=4, required=True, unique=True)
+    email = db.EmailField(max_length=128, min_length=4, unique=True, required=True)
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+class Form(db.Document):
+    _id = db.StringField(primary_key=True, required=True) #mongodb autoimplemnt this field
+    answer_1 = db.BooleanField(required=True)
+    answer_2 = db.BooleanField(required=True)
+    answer_3 = db.BooleanField(required=True)
+    answer_4 = db.BooleanField(required=True)
+    answer_5 = db.BooleanField(required=True)
+    answer_6 = db.BooleanField(required=True)
+    answer_7 = db.BooleanField(required=True)
+    answer_8 = db.BooleanField(required=True)
+    answer_9 = db.BooleanField(required=True)
+    answer_10 = db.BooleanField(required=True)
+    answer_11 = db.BooleanField(required=True)
+    timestamp = db.DateTimeField(default=datetime.utcnow, required=True)
+    user_id = db.StringField(unique=True, required=True)
 
 if __name__ == "__main__":
     user = User()
@@ -17,3 +35,20 @@ if __name__ == "__main__":
     user.password_hash = "1234"
     user.email = "dan@email.com"
     user.save()
+
+    form = Form(
+        _id=uuid.uuid4().hex,
+        answer_1=True,
+        answer_2=True,
+        answer_3=True,
+        answer_4=True,
+        answer_5=True,
+        answer_6=True,
+        answer_7=True,
+        answer_8=True,
+        answer_9=True,
+        answer_10=True,
+        answer_11=True,
+        user_id=user._id
+    )
+    form.save()
