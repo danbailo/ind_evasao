2430c043c8e3990e62a6ee7f7496689d718d2ec7
Daniel Bailo <danbailoufms@gmail.com>
2020-11-13 22:29:58 -0300

    added loginmanager and point to login_view to use the decorator that need to be logged

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config())
 db = MongoEngine(app)
+login = LoginManager(app)
+login.login_view = "login"
 
 from app import routes, models
 
