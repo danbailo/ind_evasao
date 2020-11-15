bea42e4f4de6d9612c77d0bd624c8215f8cec51c
Daniel Bailo <danbailoufms@gmail.com>
2020-11-15 13:02:13 -0300

    added env vars config

--- src/config.py
+++ src/config.py
@@ -1,8 +1,13 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MONGODB_SETTINGS = os.environ.get("DATABASE_URL") or\
-    {
-        'db': 'ind_evasao',
-    }
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    MONGODB_DB = os.environ.get("MONGODB_DB")
+    MONGODB_HOST = os.environ.get("MONGODB_HOST")
+    MONGODB_PORT = int(os.environ.get("MONGODB_PORT"))
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = ["danbailoufms@gmail.com"]
