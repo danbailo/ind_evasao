7ae403bd00cc92a282918b5c4ce31b34f598dacd
Daniel Bailo <danbailoufms@gmail.com>
2020-11-15 13:01:42 -0300

    implement a production feature to sent a message to admin report an error of the system

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+import logging
+from logging.handlers import SMTPHandler
+
 from config import Config
-from flask_mongoengine import MongoEngine
+from flask import Flask
 from flask_login import LoginManager
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,8 +12,25 @@ db = MongoEngine(app)
 login = LoginManager(app)
 login.login_view = "login"
 
-from app import routes, models, errors
+from app import errors, models, routes
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': models.User, 'Form': models.Form}
+    return {"db": db, "User": models.User, "Form": models.Form}
+
+# sent a message to admin email reporting a database error for example
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+            toaddrs=app.config["ADMINS"], subject="Índice de Evasão - Falha no sistema!",
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
