ec770544b39d723fc40352006428c99c4319e00b
Daniel Bailo <danbailoufms@gmail.com>
2020-11-15 15:03:24 -0300

    methods to generate a jwt token and verify if the token is valid, respectively

--- src/app/models.py
+++ src/app/models.py
@@ -1,9 +1,13 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
-from datetime import datetime
 import uuid
+from datetime import datetime
+from time import time
+
+import jwt
 from flask_login import UserMixin
-from app import login
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import app, db, login
+
 
 class User(UserMixin, db.Document):
     _id = db.StringField(primary_key=True,required=True) #mongodb autoimplemnt this field
@@ -20,6 +24,20 @@ class User(UserMixin, db.Document):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {"reset_password": self._id, "exp": time() + expires_in},
+            app.config["SECRET_KEY"], algorithm="HS256").decode("utf-8")
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            _id = jwt.decode(token, app.config["SECRET_KEY"],
+                            algorithms=["HS256"])["reset_password"]
+        except:
+            return None
+        return User.objects(_id=_id).first()
 
     def __repr__(self):
         return f"<User {self.username}>"
