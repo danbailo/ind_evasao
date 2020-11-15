721d228346eb58fc894c0650308a9633de0342b1
Daniel Bailo <danbailoufms@gmail.com>
2020-11-15 10:02:59 -0300

    created register view

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 from flask_login import current_user, login_user, logout_user
 from flask_login import login_required
 from app.models import User
 from flask import request
 from werkzeug.urls import url_parse
+from app import db
+import uuid
 
 @app.route("/")
 @app.route("/index")
@@ -36,3 +38,19 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(name=form.name.data,
+                    username=form.username.data, 
+                    email=form.email.data)
+        user.set_password(form.password.data)
+        user.set_id(uuid.uuid4().hex)
+        user.save()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template("register.html", title="Register", form=form)
