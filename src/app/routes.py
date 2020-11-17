b912354ded82fb270c68dddce9f1c523a7752e77
Daniel Bailo <danbailoufms@gmail.com>
2020-11-16 21:25:36 -0300

    implemented the index page with login and register form

--- src/app/routes.py
+++ src/app/routes.py
@@ -13,49 +13,53 @@ from app.models import User
 
 @app.route("/")
 @app.route("/index")
-@login_required
 def index():
-    return render_template("index.html", title="Home")
+    login_form = LoginForm()
+    register_form = RegisterForm()
+    return render_template("index.html", title="Home", login_form=login_form, register_form=register_form)
 
-@app.route("/login", methods=["GET", "POST"]) # allow methods GET and POST to this view
+
+@app.route("/login", methods=["POST"]) # allow methods GET and POST to this view
 def login():
     if current_user.is_authenticated: # if user try to navigates to /login
         return redirect(url_for("index"))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.objects(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
+    register_form = RegisterForm()
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.objects(username=login_form.username.data).first()
+        if user is None or not user.check_password(login_form.password.data):
+            flash('Usuário ou senha inválidos')
+            return redirect(url_for('index'))
+        login_user(user, remember=login_form.remember_me.data)
         # when an user try to access a content that needs login
         # its necessary get the url content to redirect it to the next page
         next_page = request.args.get('next') 
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)        
-    return render_template("login.html", title="Sign In", form=form)
+    return render_template("index.html", login_form=login_form, register_form=register_form)
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("index"))
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["POST"])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for("index"))
-    form = RegisterForm()
-    if form.validate_on_submit():
-        user = User(name=form.name.data,
-                    username=form.username.data, 
-                    email=form.email.data)
-        user.set_password(form.password.data)
+    register_form = RegisterForm()
+    login_form = LoginForm()
+    if register_form.validate_on_submit():
+        user = User(name=register_form.name.data,
+                    username=register_form.username.data, 
+                    email=register_form.email.data)
+        user.set_password(register_form.password.data)
         user.set_id(uuid.uuid4().hex)
         user.save()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template("register.html", title="Register", form=form)
+        flash('Parabéns, agora você é um usuário registrado!')
+        return redirect(url_for('index'))
+    return render_template("index.html", register_form=register_form, login_form=login_form)
 
 @app.route("/reset_password_request", methods=["GET", "POST"])
 def reset_password_request():
